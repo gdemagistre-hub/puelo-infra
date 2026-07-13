a60b3d4541461c9d82ffb28906ebde91c5a25935
gdemagistre-hub <gdemagistre@gmail.com>
2026-07-13 17:48:36 -0300

    Create buscadorPrestadores.dart

---
+++ lib/lib/lib/buscadorPrestadores.dart
@@ -0,0 +1,88 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'buscador_prestadores_model.dart';
+export 'buscador_prestadores_model.dart';
+
+class BuscadorPrestadoresWidget extends StatefulWidget {
+  const BuscadorPrestadoresWidget({super.key});
+
+  static String routeName = 'buscadorPrestadores';
+  static String routePath = '/buscadorPrestadores';
+
+  @override
+  State<BuscadorPrestadoresWidget> createState() =>
+      _BuscadorPrestadoresWidgetState();
+}
+
+class _BuscadorPrestadoresWidgetState extends State<BuscadorPrestadoresWidget> {
+  late BuscadorPrestadoresModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => BuscadorPrestadoresModel());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        appBar: AppBar(
+          backgroundColor: FlutterFlowTheme.of(context).primary,
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Prestadores',
+            style: FlutterFlowTheme.of(context).headlineMedium.override(
+                  font: GoogleFonts.interTight(
+                    fontWeight:
+                        FlutterFlowTheme.of(context).headlineMedium.fontWeight,
+                    fontStyle:
+                        FlutterFlowTheme.of(context).headlineMedium.fontStyle,
+                  ),
+                  color: Colors.white,
+                  fontSize: 22,
+                  letterSpacing: 0.0,
+                  fontWeight:
+                      FlutterFlowTheme.of(context).headlineMedium.fontWeight,
+                  fontStyle:
+                      FlutterFlowTheme.of(context).headlineMedium.fontStyle,
+                ),
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 2,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
