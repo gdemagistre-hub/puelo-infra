71341d635d615197a833a5815d26046ae21e7877
gdemagistre-hub <gdemagistre@gmail.com>
2026-07-15 15:02:09 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
-// 1. Agregar la importación arriba de todo junto a las demás:
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'splashScreen.dart';
+import 'Homepage.dart'; 
+import 'registroTrabajador.dart';
+import 'buscadorPrestadores.dart';
+import 'tarjetaDigital.dart';
 
-// ... tu código de inicialización de Firebase ...
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAr6iPh8NaDBD4qwo3LvfpE4j9k7RfKTwQ",
+      authDomain: "lifewalletpuelo.firebaseapp.com",
+      projectId: "lifewalletpuelo",
+      storageBucket: "lifewalletpuelo.firebasestorage.app",
+      messagingSenderId: "74624927314",
+      appId: "1:74624927314:web:3fadcc533dd1f3a985818b",
+    ),
+  );
+  
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,7 +37,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFF8FAFC),
         useMaterial3: true,
       ),
-      // 2. Establecer la pantalla de bienvenida como la inicial
+      // La pantalla inicial ahora será el SplashScreen
       initialRoute: SplashScreenWidget.routePath,
       routes: {
         SplashScreenWidget.routePath: (context) => const SplashScreenWidget(),
@@ -24,8 +45,27 @@ class MyApp extends StatelessWidget {
         RegistroTrabajadorWidget.routePath: (context) => const RegistroTrabajadorWidget(),
         BuscadorPrestadoresWidget.routePath: (context) => const BuscadorPrestadoresWidget(),
       },
+      // Manejo dinámico para la carga y lectura de la Tarjeta Digital
       onGenerateRoute: (settings) {
-        // ... tu lógica existente de onGenerateRoute para la tarjeta digital ...
+        final settingsName = settings.name ?? '';
+        final uri = Uri.parse(settingsName);
+
+        if (uri.path == TarjetaDigitalWidget.routePath) {
+          DocumentReference? userRef;
+
+          if (settings.arguments is Map<String, dynamic>) {
+            final args = settings.arguments as Map<String, dynamic>;
+            userRef = args['usuarioRef'] as DocumentReference?;
+          } else if (settings.arguments is DocumentReference) {
+            userRef = settings.arguments as DocumentReference?;
+          }
+
+          return MaterialPageRoute(
+            settings: settings,
+            builder: (context) => TarjetaDigitalWidget(usuarioRef: userRef),
+          );
+        }
+        return null;
       },
     );
   }
