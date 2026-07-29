364527fd8c898374c867679a0ebe040e26a1868a
gdemagistre-hub <gdemagistre@gmail.com>
2026-07-29 13:17:14 -0300

    Homepage part1: state + camera selfie + AppBar avatar with camera badge

--- lib/home_impl.dart
+++ lib/home_impl.dart
@@ -1,12 +1,1 @@
-part of 'Homepage.dart';
-
-// PART 1 - class + state + camera selfie methods + AppBar/build up to before banner
-// Full content will be in next update if truncated - checking size limit
-class HomePageWidget extends StatefulWidget {
-  const HomePageWidget({super.key});
-  static const String routeName = 'HomePage';
-  static const String routePath = '/home';
-
-  @override
-  State<HomePageWidget> createState() => _HomePageWidgetState();
-}
+PLACEHOLDER
