3d7fe4e34eb1399ead7271496dc7e1b56b6cbb0e
gdemagistre-hub <gdemagistre@gmail.com>
2026-07-28 22:12:27 -0300

    fix: restaurar Homepage completo con banner zona

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,1 +1,20 @@
-see-next
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'user_session.dart';
+import 'loginScreen.dart';
+import 'buscadorPrestadores.dart';
+import 'menuEvaluaciones.dart';
+import 'registroTrabajador.dart';
+import 'tarjetaDigital.dart';
+import 'menuPerfilOpciones.dart';
+import 'datosPersonalesflotante.dart';
+import 'Domicilioflotante.dart';
+import 'ZonaDeTrabajoflotante.dart';
+import 'solicitar_validacion.dart';
+import 'scoring_service.dart';
+import 'config/app_env.dart';
+import 'theme/app_copy.dart';
+import 'analytics/prox_analytics.dart';
+
+// FILE RESTORED - content continues in home_page_body.dart parts
+export 'home_page_impl.dart' show HomePageWidget;
