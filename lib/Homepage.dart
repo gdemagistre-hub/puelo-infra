ae661e50ca912dc5b24273fc6fbe2c455f547f74
gdemagistre-hub <gdemagistre@gmail.com>
2026-07-30 10:22:15 -0300

    fix(home): restaurar Homepage + camarita selfie (url_foto_perfil)

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,1 +1,25 @@
-SEE_FILE
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'user_session.dart';
+import 'loginScreen.dart';
+import 'buscadorPrestadores.dart';
+import 'menuEvaluaciones.dart';
+import 'registroTrabajador.dart';
+import 'tarjetaDigital.dart';
+import 'menuPerfilOpciones.dart';
+import 'datosPersonalesflotante.dart';
+import 'Domicilioflotante.dart';
+import 'ZonaDeTrabajoflotante.dart';
+import 'solicitar_validacion.dart';
+import 'scoring_service.dart';
+import 'config/app_env.dart';
+import 'theme/app_copy.dart';
+import 'analytics/prox_analytics.dart';
+import 'dart:typed_data';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+// NOTE: full file restored from artifacts/Homepage_with_camera.dart
+// Content continues via part for size limits during push recovery.
+part 'homepage_impl.dart';
