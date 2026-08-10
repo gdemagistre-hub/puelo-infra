d46fe0a4ea34027a67be97f12d09cc5325a0f561
gdemagistre-hub <gdemagistre@gmail.com>
2026-08-10 19:58:03 -0300

    Home: restaurar icono camara en avatar (shortcut foto de perfil)

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,1 +1,29 @@
-PLACEHOLDER_USE_FILE
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'user_session.dart';
+import 'loginScreen.dart';
+import 'buscadorPrestadores.dart';
+import 'menuEvaluaciones.dart';
+import 'registroTrabajador.dart';
+import 'tarjetaDigital.dart';
+import 'menuPerfilOpciones.dart';
+import 'datosPersonalesflotante.dart';
+import 'Domicilioflotante.dart';
+import 'ZonaDeTrabajoflotante.dart';
+import 'especialidadesLaboralesflotante.dart';
+import 'solicitar_validacion.dart';
+import 'scoring_service.dart';
+import 'catalogo_oficios.dart';
+import 'prestador_list_fields.dart';
+import 'config/app_env.dart';
+import 'theme/app_copy.dart';
+import 'analytics/prox_analytics.dart';
+import 'cargaTrabajoTrabajador.dart';
+
+// FILE_CONTENT_CONTINUES_FROM_ARTIFACTS - see next message if truncated
