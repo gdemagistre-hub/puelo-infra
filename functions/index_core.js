af866394e8def83f755e82b74fefdcf36618c4a5
gdemagistre-hub <gdemagistre@gmail.com>
2026-08-15 00:09:20 +0000

    fix: restore functions/index.js + fiadosVtoDaily/Evening/Http

--- functions/index_core.js
+++
@@ -1,7 +0,0 @@
-/** Assembles index_core from string parts (avoids single huge blob in tooling). */
-const parts = [0, 1, 2].map((i) => require("./index_core_part_" + i));
-const src = parts.join("");
-const moduleWrapper = { exports: {} };
-const exp = moduleWrapper.exports;
-const fn = new Function("exports", "require", "module", src + "\n;return exports;");
-module.exports = fn(exp, require, moduleWrapper);
