b6775b41e39d3eb0e1aba8bf130f49a14e5e4849
gdemagistre-hub <gdemagistre@gmail.com>
2026-08-20 18:47:11 -0300

    fix(home): Homepage completo + showcaseview 5.1 tour anclado a widgets

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,1 +1,12 @@
-PLACEHOLDER
+import 'package:flutter/material.dart';
+// TEMP - will be replaced
+class HomePageWidget extends StatelessWidget {
+  final bool? initialModoPrestador;
+  const HomePageWidget({super.key, this.initialModoPrestador});
+  static const String routeName = 'HomePage';
+  static const String routePath = '/home';
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Restaurando Home...')));
+  }
+}
