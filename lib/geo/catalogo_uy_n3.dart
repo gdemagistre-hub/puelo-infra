333493ee6843858649f568e4488ce2a6dfe65070
gdemagistre-hub <gdemagistre@gmail.com>
2026-09-01 01:21:59 -0300

    feat(geo): embeber 615 localidades UY 2011 (kUyNivel3Raw)

---
+++ lib/geo/catalogo_uy_n3.dart
@@ -0,0 +1,617 @@
+const String kUyNivel3Raw = r'''
+UY-1020|Montevideo|01|01
+UY-2220|Artigas|02|02
+UY-2521|Bella Union|02|02
+UY-2522|Tomas Gomensoro|02|02
+UY-2621|Baltasar Brum|02|02
+UY-2721|Bernabe Rivera|02|02
+UY-2724|Franquia|02|02
+UY-2725|Sequeira|02|02
+UY-2822|Coronado|02|02
+UY-2823|Cuareim|02|02
+UY-2824|Cuaro|02|02
+UY-2826|Javier de Viana|02|02
+UY-2827|La Bolsa|02|02
+UY-2828|Las Piedras|02|02
+UY-2830|Pintadito|02|02
+UY-2831|Port. de Hierro y Campodonico|02|02
+UY-2833|Mones Quintela|02|02
+UY-2925|Cainsa|02|02
+UY-2927|Paso Campamento|02|02
+UY-2934|Diego Lamas|02|02
+UY-2945|Paso Farias|02|02
+UY-2952|Rincon de Pacheco|02|02
+UY-2957|Topador|02|02
+UY-2958|Cerro Ejido|02|02
+UY-2959|Colonia Palma|02|02
+UY-2961|Cerro Signorelli (el Mirador)|02|02
+UY-2962|Cerro San Eugenio|02|02
+UY-2963|Calnu|02|02
+UY-3221|Las Piedras|03|03
+UY-3320|Canelones|03|03
+UY-3321|La Paz|03|03
+UY-3322|Pando|03|03
+UY-3323|Santa Lucia|03|03
+UY-3324|Piedras de Afilar|03|03
+UY-3325|Cumbres de Carrasco|03|03
+UY-3326|Haras del Lago|03|03
+UY-3327|Quinta los Horneros|03|03
+UY-3328|Las Higueritas|03|03
+UY-3329|Sofia Santos|03|03
+UY-3421|Progreso|03|03
+UY-3422|San Ramon|03|03
+UY-3521|Barros Blancos|03|03
+UY-3523|Colonia Nicolich|03|03
+UY-3524|Joaquin Suarez|03|03
+UY-3525|Paso Carrasco|03|03
+UY-3526|Santa Rosa|03|03
+UY-3527|Sauce|03|03
+UY-3528|Tala|03|03
+UY-3529|Villa Crespo y San Andres|03|03
+UY-3530|Fracc. Cno. Andaluz y R.84|03|03
+UY-3621|Atlantida|03|03
+UY-3622|Estacion Atlantida|03|03
+UY-3624|Cerrillos|03|03
+UY-3625|Empalme Olmos|03|03
+UY-3626|Migues|03|03
+UY-3627|Parque del Plata|03|03
+UY-3628|San Bautista|03|03
+UY-3629|San Jacinto|03|03
+UY-3630|Dr. Francisco Soca|03|03
+UY-3631|Toledo|03|03
+UY-3632|Montes|03|03
+UY-3633|San Jose de Carrasco|03|03
+UY-3634|Fracc. Sobre Ruta 74|03|03
+UY-3721|Aguas Corrientes|03|03
+UY-3722|Barra de Carrasco|03|03
+UY-3723|Juanico|03|03
+UY-3724|La Floresta|03|03
+UY-3725|Estacion la Floresta|03|03
+UY-3726|Las Toscas|03|03
+UY-3727|Parque Carrasco|03|03
+UY-3729|Salinas|03|03
+UY-3730|San Antonio|03|03
+UY-3731|Aeropuerto Internacional de Carras|03|03
+UY-3732|Solymar|03|03
+UY-3733|Villa Aeroparque|03|03
+UY-3823|Castellanos|03|03
+UY-3825|Barrio Copola|03|03
+UY-3826|Costa Azul|03|03
+UY-3827|Costa y Guillamon|03|03
+UY-3828|El Pinar|03|03
+UY-3829|Estacion Migues|03|03
+UY-3830|Pinamar - Pinepark|03|03
+UY-3831|Lagomar|03|03
+UY-3832|Olmos|03|03
+UY-3833|Parada Cabrera|03|03
+UY-3834|San Luis|03|03
+UY-3835|Shangrila|03|03
+UY-3836|Totoral del Sauce|03|03
+UY-3837|Villa Felicidad|03|03
+UY-3838|Villa Paz S.a.|03|03
+UY-3839|Villa San Jose|03|03
+UY-3840|Estacion Tapia|03|03
+UY-3880|Villa San Felipe|03|03
+UY-3881|Villa Hadita|03|03
+UY-3882|Paso de Pache|03|03
+UY-3910|City Golf|03|03
+UY-3911|Viejo Molino San Bernardo|03|03
+UY-3912|Estanque de Pando|03|03
+UY-3913|Jardines de Pando|03|03
+UY-3914|Paso Espinosa|03|03
+UY-3921|Araminda|03|03
+UY-3922|Argentino|03|03
+UY-3925|Bello Horizonte|03|03
+UY-3926|Biarritz|03|03
+UY-3927|Bolivar|03|03
+UY-3928|Campo Militar|03|03
+UY-3929|Capilla de Cella|03|03
+UY-3934|Cruz de los Caminos|03|03
+UY-3935|Cuchilla Alta|03|03
+UY-3939|El Bosque|03|03
+UY-3942|Estacion Pedrera|03|03
+UY-3943|Fortin de Santa Rosa|03|03
+UY-3944|Fracc. Progreso|03|03
+UY-3945|Instituto Adventista|03|03
+UY-3946|Jaureguiberry|03|03
+UY-3948|La Lucha|03|03
+UY-3949|La Montañesa|03|03
+UY-3953|Lomas de Solymar|03|03
+UY-3956|Los Titanes|03|03
+UY-3957|Marindia|03|03
+UY-3959|Neptunia|03|03
+UY-3962|Paso de la Cadena|03|03
+UY-3966|Paso Palomeque|03|03
+UY-3968|Piedra del Toro|03|03
+UY-3969|Estacion Piedras de Afilar|03|03
+UY-3970|El Galeon|03|03
+UY-3974|Santa Ana|03|03
+UY-3975|Santa Lucia del Este|03|03
+UY-3978|Seis Hermanos|03|03
+UY-3979|Villa Arejo|03|03
+UY-3980|Villa Argentina|03|03
+UY-3984|Villa Porvenir|03|03
+UY-3985|La Tuna|03|03
+UY-3986|Guazu - Vira|03|03
+UY-3987|Colinas de Solymar|03|03
+UY-3988|Barrio Remanso|03|03
+UY-3989|Villa El Tato|03|03
+UY-3990|Villa San Cono|03|03
+UY-3991|Villa Juana|03|03
+UY-3992|Colinas de Carrasco|03|03
+UY-3993|Lomas de Carrasco|03|03
+UY-3994|Carmel|03|03
+UY-3995|La Asuncion|03|03
+UY-3996|Quintas del Bosque|03|03
+UY-3997|Altos de la Tahona|03|03
+UY-4220|Melo|04|04
+UY-4521|Fraile Muerto|04|04
+UY-4522|Rio Branco|04|04
+UY-4621|Tupambae|04|04
+UY-4721|Isidoro Noblia|04|04
+UY-4821|Acegua|04|04
+UY-4822|Bañado de Medina|04|04
+UY-4823|Centurion|04|04
+UY-4824|Cerro de las Cuentas|04|04
+UY-4825|Hipodromo|04|04
+UY-4827|Placido Rosas|04|04
+UY-4828|Toledo|04|04
+UY-4829|Tres Islas|04|04
+UY-4830|Poblado Uruguay|04|04
+UY-4923|Arbolito|04|04
+UY-4924|Arevalo|04|04
+UY-4930|Caserio las Cañas|04|04
+UY-4940|Esperanza|04|04
+UY-4943|Getulio Vargas|04|04
+UY-4948|La Pedrera|04|04
+UY-4949|Lago Merin|04|04
+UY-4952|Mangrullo|04|04
+UY-4956|Nando|04|04
+UY-4966|Quebracho|04|04
+UY-4968|Ramon Trigo|04|04
+UY-4981|Soto Goro|04|04
+UY-4983|Barrio Lopez Benitez|04|04
+UY-4987|Barrio la Vinchuca|04|04
+UY-4988|Arachania|04|04
+UY-4989|Ñangapire|04|04
+UY-5320|Colonia del Sacramento|05|05
+UY-5321|Carmelo|05|05
+UY-5323|Juan Lacaze|05|05
+UY-5421|Nueva Helvecia|05|05
+UY-5422|Rosario|05|05
+UY-5423|Nueva Palmira|05|05
+UY-5452|Paso Antolin|05|05
+UY-5521|Ombues de Lavalle|05|05
+UY-5522|Tarariras|05|05
+UY-5621|Colonia Valdense|05|05
+UY-5622|Florencio Sanchez|05|05
+UY-5721|Conchillas|05|05
+UY-5722|Caserio El Cerro|05|05
+UY-5724|La Paz|05|05
+UY-5726|Riachuelo|05|05
+UY-5727|Juan Carlos Caseros|05|05
+UY-5798|Agraciada|05|05
+UY-5821|Boca del Rosario|05|05
+UY-5824|Cufre|05|05
+UY-5826|El Semillero|05|05
+UY-5827|Estacion Estanzuela|05|05
+UY-5829|Cerros de San Juan|05|05
+UY-5831|Miguelete|05|05
+UY-5923|Campana|05|05
+UY-5924|Artilleros|05|05
+UY-5925|Barker|05|05
+UY-5927|El Ensueño|05|05
+UY-5931|Blanca Arena|05|05
+UY-5932|Brisas del Plata|05|05
+UY-5938|Colonia Cosmopolita|05|05
+UY-5940|Paraje Minuano|05|05
+UY-5951|Los Pinos|05|05
+UY-5955|Chico Torino|05|05
+UY-5956|La Horqueta|05|05
+UY-5959|Playa Azul|05|05
+UY-5960|Playa Britopolis|05|05
+UY-5961|Playa Parant|05|05
+UY-5962|Playa Fomento|05|05
+UY-5964|Puerto Ingles|05|05
+UY-5969|Radial Hernandez|05|05
+UY-5977|San Pedro|05|05
+UY-5979|Santa Ana|05|05
+UY-5980|Santa Regina|05|05
+UY-5987|Zagarzazu|05|05
+UY-5989|Arrivillaga|05|05
+UY-5993|El Faro|05|05
+UY-5994|Laguna de los Patos|05|05
+UY-5996|Juan Jackson|05|05
+UY-5997|Pueblo Gil|05|05
+UY-5998|Cerro Carmelo|05|05
+UY-5999|El Quinton|05|05
+UY-6220|Durazno|06|06
+UY-6421|Sarandi del Yi|06|06
+UY-6521|Carmen|06|06
+UY-6621|Blanquillo|06|06
+UY-6622|La Paloma|06|06
+UY-6721|Carlos Reyles|06|06
+UY-6722|Centenario|06|06
+UY-6724|Santa Bernardina|06|06
+UY-6790|Cerro Chato|06|06
+UY-6821|Baygorria|06|06
+UY-6922|Aguas Buenas|06|06
+UY-6924|Pueblo de Alvarez|06|06
+UY-6940|Feliciano|06|06
+UY-6951|Ombues de Oribe|06|06
+UY-6960|Rossell y Rius|06|06
+UY-6964|San Jorge|06|06
+UY-6972|Las Palmas|06|06
+UY-7320|Trinidad|07|07
+UY-7721|Ismael Cortinas|07|07
+UY-7921|Andresito|07|07
+UY-7925|Juan Jose Castro|07|07
+UY-7937|La Casilla|07|07
+UY-7938|Cerro Colorado|07|07
+UY-8220|Florida|08|08
+UY-8421|Sarandi Grande|08|08
+UY-8521|Casupa|08|08
+UY-8621|Cardal|08|08
+UY-8622|Fray Marcos|08|08
+UY-8623|25 de Agosto|08|08
+UY-8624|25 de Mayo|08|08
+UY-8721|Alejandro Gallinal|08|08
+UY-8722|Capilla del Sauce|08|08
+UY-8723|La Cruz|08|08
+UY-8724|Nico Perez|08|08
+UY-8790|Cerro Chato|08|08
+UY-8821|Chamizo|08|08
+UY-8822|Goñi|08|08
+UY-8823|Mendoza|08|08
+UY-8824|Mendoza Chico|08|08
+UY-8825|Reboledo|08|08
+UY-8896|Valentines|08|08
+UY-8923|Berrondo|08|08
+UY-8930|Pueblo Ferrer|08|08
+UY-8933|Independencia|08|08
+UY-8938|Montecoral|08|08
+UY-8940|Pintado|08|08
+UY-8941|Polanco del Yi|08|08
+UY-8944|Puntas de Maciel|08|08
+UY-8991|Illescas|08|08
+UY-8992|Caserio la Fundacion|08|08
+UY-8993|La Macana|08|08
+UY-8994|Estacion Capilla del Sauce|08|08
+UY-8995|San Gabriel|08|08
+UY-9220|Minas|09|09
+UY-9521|Jose Batlle y Ordoñez|09|09
+UY-9522|Jose Pedro Varela|09|09
+UY-9621|Mariscala|09|09
+UY-9622|Solis de Mataojo|09|09
+UY-9721|Piraraja|09|09
+UY-9722|Zapican|09|09
+UY-9821|Colon|09|09
+UY-9823|Aramendia|09|09
+UY-9924|Blanes Viale|09|09
+UY-9928|19 de Junio|09|09
+UY-9931|Estacion Solis|09|09
+UY-9932|Gaetan|09|09
+UY-9942|Polanco Norte|09|09
+UY-9955|Villa del Rosario|09|09
+UY-9956|Villa Serrana|09|09
+UY-9957|Barrio la Coronilla - Ancap|09|09
+UY-9958|San Francisco de las Sierras|09|09
+UY-9991|Illescas|09|09
+UY-10320|Maldonado|10|10
+UY-10321|San Carlos|10|10
+UY-10521|Aigua|10|10
+UY-10522|Pan de Azucar|10|10
+UY-10523|Piriapolis|10|10
+UY-10524|Punta del Este|10|10
+UY-10821|Cerro Pelado|10|10
+UY-10822|Garzon|10|10
+UY-10823|Gerona|10|10
+UY-10825|Las Flores - Estacion|10|10
+UY-10826|Los Talas|10|10
+UY-10827|Nueva Carrara|10|10
+UY-10828|Solis|10|10
+UY-10829|Pueblo Solis|10|10
+UY-10830|Pinares - las Delicias|10|10
+UY-10831|Chihuahua|10|10
+UY-10832|Villa Delia|10|10
+UY-10833|San Rafael - El Placer|10|10
+UY-10924|Barrio Hipodromo|10|10
+UY-10925|Los Aromos|10|10
+UY-10926|Bella Vista|10|10
+UY-10929|Canteras de Marelli|10|10
+UY-10931|Cerros Azules|10|10
+UY-10934|El Chorro|10|10
+UY-10935|El Eden|10|10
+UY-10936|El Tesoro|10|10
+UY-10939|Faro Jose Ignacio|10|10
+UY-10940|Gregorio Aznarez|10|10
+UY-10942|La Barra|10|10
+UY-10944|La Capuera|10|10
+UY-10945|Las Flores|10|10
+UY-10947|Manantiales|10|10
+UY-10948|Ocean Park|10|10
+UY-10953|Playa Grande|10|10
+UY-10954|Playa Hermosa|10|10
+UY-10955|Playa Verde|10|10
+UY-10956|Punta Ballena|10|10
+UY-10958|Punta Colorada|10|10
+UY-10959|Punta Negra|10|10
+UY-10962|Ruta 37 y 9|10|10
+UY-10969|Sauce de Portezuelo|10|10
+UY-10972|San Vicente|10|10
+UY-10973|Balneario Buenos Aires|10|10
+UY-10980|Las Cumbres|10|10
+UY-10981|Los Corchos|10|10
+UY-10982|Santa Monica|10|10
+UY-10983|Eden Rock|10|10
+UY-10984|Parque Medina|10|10
+UY-10985|Arenas de Jose Ignacio|10|10
+UY-10986|La Sonrisa|10|10
+UY-10987|El Quijote|10|10
+UY-10988|Laguna Blanca|10|10
+UY-11120|Paysandu|11|11
+UY-11521|Guichon|11|11
+UY-11522|Nuevo Paysandu|11|11
+UY-11621|Quebracho|11|11
+UY-11695|Tambores|11|11
+UY-11721|Lorenzo Geyres|11|11
+UY-11722|Merinos|11|11
+UY-11723|Porvenir|11|11
+UY-11821|Arbolito|11|11
+UY-11822|Beisso|11|11
+UY-11823|Casablanca|11|11
+UY-11824|Cerro Chato|11|11
+UY-11825|Constancia|11|11
+UY-11826|Morato|11|11
+UY-11827|Piedras Coloradas|11|11
+UY-11828|Piñera|11|11
+UY-11830|San Felix|11|11
+UY-11831|Villa Maria (tiatucura)|11|11
+UY-11894|Piedra Sola|11|11
+UY-11922|Araujo|11|11
+UY-11924|Bella Vista|11|11
+UY-11925|Cañada del Pueblo|11|11
+UY-11926|Chapicuy|11|11
+UY-11929|El Eucaliptus|11|11
+UY-11931|Esperanza|11|11
+UY-11932|Pueblo Federacion|11|11
+UY-11935|La Tentacion|11|11
+UY-11937|Orgoroso|11|11
+UY-11945|Cuchilla de Buricayupi|11|11
+UY-11947|Soto|11|11
+UY-11950|Zeballos|11|11
+UY-11951|Chacras de Paysandu|11|11
+UY-11953|Gallinal|11|11
+UY-11954|Puntas de Arroyo Negro|11|11
+UY-11955|Estacion Porvenir|11|11
+UY-11956|Cuchilla de Fuego|11|11
+UY-11957|Pueblo Alonzo|11|11
+UY-11958|Queguayar|11|11
+UY-11959|Termas de Guaviyu|11|11
+UY-11960|Termas de Almiron|11|11
+UY-12320|Fray Bentos|12|12
+UY-12421|Young|12|12
+UY-12621|Nuevo Berlin|12|12
+UY-12622|San Javier|12|12
+UY-12722|Barrio Anglo|12|12
+UY-12723|Grecco|12|12
+UY-12792|Merinos|12|12
+UY-12797|Algorta|12|12
+UY-12821|El Ombu|12|12
+UY-12823|Paso de los Mellizos|12|12
+UY-12824|Sarandi de Navarro|12|12
+UY-12826|Villa General Borges|12|12
+UY-12827|Villa Maria|12|12
+UY-12828|Las Cañas|12|12
+UY-12923|Bellaco|12|12
+UY-12930|Los Arrayanes|12|12
+UY-12932|Menafra|12|12
+UY-12945|Tres Quintas|12|12
+UY-13220|Rivera|13|13
+UY-13521|Minas de Corrales|13|13
+UY-13522|Tranqueras|13|13
+UY-13621|Vichadero|13|13
+UY-13721|Santa Teresa|13|13
+UY-13822|Arroyo Blanco|13|13
+UY-13823|Paso Ataques|13|13
+UY-13824|Cerro Pelado|13|13
+UY-13830|Paso Hospital|13|13
+UY-13831|Lapuente|13|13
+UY-13832|Las Flores|13|13
+UY-13833|Moirones|13|13
+UY-13836|La Pedrera|13|13
+UY-13837|Mandubi|13|13
+UY-13838|Lagunon|13|13
+UY-13922|Amarillo|13|13
+UY-13929|Cerrillada|13|13
+UY-13931|Cerros de la Calera|13|13
+UY-13940|Lagos del Norte|13|13
+UY-13943|Masoller|13|13
+UY-14320|Rocha|14|14
+UY-14421|Castillos|14|14
+UY-14422|Lascano|14|14
+UY-14521|Chuy|14|14
+UY-14621|Cebollati|14|14
+UY-14622|Velazquez|14|14
+UY-14721|18 de Julio|14|14
+UY-14722|La Paloma|14|14
+UY-14723|San Luis Al Medio|14|14
+UY-14821|La Aguada y Costa Azul|14|14
+UY-14822|19 de Abril|14|14
+UY-14823|La Coronilla|14|14
+UY-14826|Barrio Pereira|14|14
+UY-14921|Aguas Dulces|14|14
+UY-14923|Barra del Chuy|14|14
+UY-14926|Barrio Torres|14|14
+UY-14928|Cabo Polonio|14|14
+UY-14929|Capacho|14|14
+UY-14940|Barra de Valizas|14|14
+UY-14941|La Esmeralda|14|14
+UY-14942|La Pedrera|14|14
+UY-14948|Paralle|14|14
+UY-14953|Puerto de los Botes|14|14
+UY-14957|Puimayen|14|14
+UY-14959|Arachania|14|14
+UY-14960|Pta. Rubia y Sta. Isabel de la Ped|14|14
+UY-14962|Punta del Diablo|14|14
+UY-14963|Palmares de la Coronilla|14|14
+UY-14964|La Ribiera|14|14
+UY-14965|Puente Valizas|14|14
+UY-14966|Oceania del Polonio|14|14
+UY-14967|Pueblo Nuevo|14|14
+UY-14968|Tajamares de la Pedrera|14|14
+UY-14969|San Antonio|14|14
+UY-15120|Salto|15|15
+UY-15522|Belen|15|15
+UY-15523|Constitucion|15|15
+UY-15721|Fernandez|15|15
+UY-15722|San Antonio|15|15
+UY-15723|Chacras de Belen|15|15
+UY-15821|Albisu|15|15
+UY-15822|Biassini|15|15
+UY-15823|Campo de Todos|15|15
+UY-15824|Cayetano|15|15
+UY-15825|Cuchilla de Guaviyu|15|15
+UY-15827|Termas del Dayman|15|15
+UY-15828|Palomas|15|15
+UY-15829|Paso del Parque del Dayman|15|15
+UY-15830|Quintana|15|15
+UY-15831|Sarandi de Arapey|15|15
+UY-15832|Saucedo|15|15
+UY-15924|Arenitas Blancas|15|15
+UY-15931|Celeste|15|15
+UY-15935|Cerros de Vera|15|15
+UY-15939|Garibaldi|15|15
+UY-15943|Termas del Arapey|15|15
+UY-15944|Las Flores|15|15
+UY-15945|Laureles|15|15
+UY-15946|Lluveras|15|15
+UY-15949|Migliaro|15|15
+UY-15950|Olivera|15|15
+UY-15957|Paso de las Piedras de Arerungua|15|15
+UY-15959|Puntas de Valentin|15|15
+UY-15965|Rincon de Valentin|15|15
+UY-15966|Colonia 18 de Julio|15|15
+UY-15969|Parque Jose Luis|15|15
+UY-15970|Colonia Itapebi|15|15
+UY-15971|Guaviyu de Arapey|15|15
+UY-15972|Russo|15|15
+UY-15973|Paso Cementerio|15|15
+UY-15974|Osimani y Llerena|15|15
+UY-16220|San Jose de Mayo|16|16
+UY-16421|Libertad|16|16
+UY-16521|Delta del Tigre y Villas|16|16
+UY-16621|Rodriguez|16|16
+UY-16721|Ituzaingo|16|16
+UY-16722|Santa Monica|16|16
+UY-16821|Puntas de Valdez|16|16
+UY-16822|Gonzalez|16|16
+UY-16823|Mal Abrigo|16|16
+UY-16824|Playa Pascual|16|16
+UY-16825|18 de Julio (pueblo Nuevo)|16|16
+UY-16826|Rafael Peraza|16|16
+UY-16827|Raigon|16|16
+UY-16829|Safici (parque Postel)|16|16
+UY-16923|Juan Soler|16|16
+UY-16924|Boca del Cufre|16|16
+UY-16925|Capurro|16|16
+UY-16926|Villa Maria|16|16
+UY-16929|Ecilda Paullier|16|16
+UY-16932|Kiyu-Ordeig|16|16
+UY-16933|La Boyada|16|16
+UY-16939|Cañada Grande|16|16
+UY-16945|Rincon del Pino|16|16
+UY-16946|San Gregorio|16|16
+UY-16947|Scavino|16|16
+UY-16950|Monte Grande|16|16
+UY-16960|Ceramicas del Sur|16|16
+UY-16961|Radial|16|16
+UY-16962|Cololo Tinosa|16|16
+UY-16963|Mangrullo|16|16
+UY-16964|Carreta Quemada|16|16
+UY-16965|Costas de Pereira|16|16
+UY-16966|Colonia Delta|16|16
+UY-17220|Mercedes|17|17
+UY-17321|Dolores|17|17
+UY-17521|Cardona|17|17
+UY-17621|Jose Enrique Rodo|17|17
+UY-17622|Palmitas|17|17
+UY-17623|Villa Soriano|17|17
+UY-17624|Palmar|17|17
+UY-17721|Egaña|17|17
+UY-17722|Santa Catalina|17|17
+UY-17798|Agraciada|17|17
+UY-17822|Castillos|17|17
+UY-17823|Risso|17|17
+UY-17824|Sacachispas|17|17
+UY-17825|Cañada Nieto|17|17
+UY-17927|Cuchilla del Perdido|17|17
+UY-17928|El Tala|17|17
+UY-17930|La Concordia|17|17
+UY-17931|La Loma|17|17
+UY-17939|Palo Solo|17|17
+UY-17950|Chacras de Dolores|17|17
+UY-17951|Colonia Concordia|17|17
+UY-17952|Perseverano|17|17
+UY-17993|Lares|17|17
+UY-18220|Tacuarembo|18|18
+UY-18321|Paso de los Toros|18|18
+UY-18521|San Gregorio de Polanco|18|18
+UY-18695|Tambores|18|18
+UY-18721|Achar|18|18
+UY-18722|Ansina|18|18
+UY-18723|Curtina|18|18
+UY-18724|Paso del Cerro|18|18
+UY-18822|Clara|18|18
+UY-18825|Cuchilla del Ombu|18|18
+UY-18827|La Hilera|18|18
+UY-18828|Las Toscas|18|18
+UY-18829|Paso Bonilla|18|18
+UY-18830|Pueblo de Arriba|18|18
+UY-18831|Pueblo del Barro|18|18
+UY-18833|Rincon del Bonete|18|18
+UY-18834|Balneario Ipora|18|18
+UY-18894|Piedra Sola|18|18
+UY-18925|Cardozo|18|18
+UY-18928|Chamberlain|18|18
+UY-18931|Cuchilla de Peralta|18|18
+UY-18937|La Pedrera|18|18
+UY-18952|Laureles|18|18
+UY-18963|Montevideo Chico|18|18
+UY-18971|Cerro de Pastoreo|18|18
+UY-18980|Sauce de Batovi|18|18
+UY-18990|Puntas de Cinco Sauces|18|18
+UY-18991|Rincon de Pereira|18|18
+UY-18992|Punta de Carretera|18|18
+UY-18993|Cruz de los Caminos|18|18
+UY-19220|Treinta y Tres|19|19
+UY-19521|Santa Clara de Olimar|19|19
+UY-19522|Vergara|19|19
+UY-19721|Arrozal Treinta y Tres|19|19
+UY-19722|Gral. Enrique Martinez|19|19
+UY-19723|Villa Sara|19|19
+UY-19790|Cerro Chato|19|19
+UY-19821|Estacion Rincon|19|19
+UY-19822|Isla Patrulla (maria Isabel)|19|19
+UY-19896|Valentines|19|19
+UY-19922|Poblado Alonzo|19|19
+UY-19924|Arrocera Rincon|19|19
+UY-19940|Arrocera los Ceibos|19|19
+UY-19941|Maria Albina|19|19
+UY-19942|Mendizabal (el Oro)|19|19
+UY-19947|Puntas del Parao|19|19
+UY-19955|Villa Passano|19|19
+UY-19956|Ejido de Treinta y Tres|19|19
+UY-19960|El Bellaco|19|19
+UY-19961|Arrocera los Teros|19|19
+UY-19962|Arrocera Bonomo|19|19
+UY-19963|Arrocera El Tigre|19|19
+UY-19964|Arrocera la Catumbera|19|19
+UY-19965|Arrocera la Querencia|19|19
+UY-19966|Arrocera las Palmas|19|19
+UY-19967|Arrocera Mini|19|19
+UY-19968|Arrocera Procipa|19|19
+UY-19969|Arrocera San Fernando|19|19
+UY-19970|Arrocera Santa Fe|19|19
+UY-19971|Arrocera Zapata|19|19
+''';
