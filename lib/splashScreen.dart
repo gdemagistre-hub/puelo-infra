d619d9d7648c2d3bc0138519e64241da970ef9fe
gdemagistre-hub <gdemagistre@gmail.com>
2026-09-02 17:26:17 -0300

    Splash Prox: logo transparente sobre fondo blanco (Flutter + Android nativo).

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -7,6 +7,7 @@ import 'elige_pais.dart';
 import 'loginScreen.dart';
 import 'user_session.dart';
 import 'theme/app_copy.dart';
+import 'branding/prox_splash_bytes.dart';
 import 'pantalla_gracias_validacion.dart';
 import 'validar_domicilio.dart';
 
@@ -139,18 +140,24 @@ class _SplashScreenWidgetState extends State<SplashScreenWidget>
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Image.asset(
-                      'assets/images/logo_prox_splash.png.png',
+                    Image.memory(
+                      kProxSplashLogoBytes,
                       width: 200,
                       fit: BoxFit.contain,
+                      gaplessPlayback: true,
                       errorBuilder: (_, __, ___) => Image.asset(
-                        'assets/images/lifewallet.png',
+                        'assets/images/logo_prox_splash.png.png',
                         width: 200,
                         fit: BoxFit.contain,
-                        errorBuilder: (_, __, ___) => const Icon(
-                          Icons.handyman_rounded,
-                          size: 80,
-                          color: Color(0xFF734BE4),
+                        errorBuilder: (_, __, ___) => Image.asset(
+                          'assets/images/lifewallet.png',
+                          width: 200,
+                          fit: BoxFit.contain,
+                          errorBuilder: (_, __, ___) => const Icon(
+                            Icons.handyman_rounded,
+                            size: 80,
+                            color: Color(0xFF734BE4),
+                          ),
                         ),
                       ),
                     ),
