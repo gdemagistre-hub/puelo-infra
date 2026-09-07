b0b281cbdc64ac0fa35ebbd2eb6c892660710d2e
gdemagistre-hub <gdemagistre@gmail.com>
2026-09-07 16:54:26 -0300

    chore: Acerca de muestra versión 1.0.3 (15)

--- lib/acerca_de.dart
+++ lib/acerca_de.dart
@@ -10,7 +10,7 @@ class AcercaDeWidget extends StatelessWidget {
 
   const AcercaDeWidget({super.key, this.modoPrestador = false});
 
-  static const String versionLabel = 'Versión 1.0.3 (14)';
+  static const String versionLabel = 'Versión 1.0.3 (15)';
   static const String copyright = 'Copyright 2026 — Puelo.app';
 
   Color get _primary =>
